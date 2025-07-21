8f5d4d4de9a349cf1d435ebc2239379fcd69b437
kirti sengar <kirtisengar95@gmail.com>
2025-07-21 15:09:32 +0530

    adding length of the description of event

--- FLAM/src/Components/CalendarApp.jsx
+++ FLAM/src/Components/CalendarApp.jsx
@@ -95,12 +95,21 @@ const CalendarApp = () => {
             {showEventPopup && (<div className="event-popup">
                 <div className="time-input">
                     <div className="event-popup-time">Time</div>
-                    <input type="number" name="hours" min={0} max={24} className="hours" value={eventTime.hours} onChange={(e) =>
+                    <input type="number" name="hours" min={0} max={24} className="hours" 
+                    value={eventTime.hours} onChange={(e) =>
                         setEventTime({...eventTime,hours:e.target.value})
                     }/>
-                    <input type="number" name="minutes" min={0} max={60} className="minutes"/>
+                    <input type="number" name="minutes" min={0} max={60} className="minutes"
+                    value={eventTime.minutes} onChange={(e) =>
+                        setEventTime({...eventTime,minutes:e.target.value})
+                    }
+                    />
                 </div>
-                <textarea placeholder="Enter Event Text (Maximum 60 Characters)"></textarea>
+                <textarea placeholder="Enter Event Text (Maximum 60 Characters)" value = {eventText} onChange={(e) =>{
+                    if(e.target.value.length <= 60){
+                        setEventText(e.target.value)
+                    }
+                }}></textarea>
                 <button className="event-popup-btn">Add Events</button>
                 <button className="close-events-popup" onClick={() => setShowEventPopup(false)}>
                     <i className="bx bx-x"></i>
